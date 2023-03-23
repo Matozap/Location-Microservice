aa85d4df54770932ccfefc918415f2272301584d
Nicolas Zapata <nicolas055@hotmail.com>
2023-03-23 09:45:40 -0500

    Added Azure Functions project

---
+++ LocationService.AFX/Functions/CountryFunction.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using LocationService.Message.Definition.Countries.Requests.v1;
+using MediatR;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace LocationService.AFX.Functions;
+
+public class CountryFunction
+{
+    private readonly ILogger _logger;
+    private readonly IMediator _mediator;
+
+    public CountryFunction(ILoggerFactory loggerFactory, IMediator mediator)
+    {
+        _mediator = mediator;
+        _logger = loggerFactory.CreateLogger<CountryFunction>();
+    }
+    
+    [Function("CountryFunction")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    {
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(await _mediator.Send(new GetAllCountries()));
+        return response;
+    }
+}
