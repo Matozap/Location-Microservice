6222e19511ab7ee5a42df89ecfafb78df98ef879
Slukad <Slukad@users.noreply.github.com>
2023-03-25 07:43:31 -0500

    Added conditional seeding for database (#7)
    * Added Azure Functions project
    
    * Added functions into API project
    
    * Fixed warning for targets and sdk
    
    * Added remaining functions
    
    * Added conditional database seeding

--- LocationService.Infrastructure/Database/Context/DatabaseOptions.cs
+++ LocationService.Infrastructure/Database/Context/DatabaseOptions.cs
@@ -5,6 +5,7 @@ public class DatabaseOptions
     public string InstanceName { get; set; }
     public string ConnectionString { get; set; }
     public string DatabaseType { get; set; }
+    public bool SeedData { get; set; }
     public EngineType EngineType => DatabaseType == "Cosmos" ? EngineType.NonRelational : EngineType.Relational;
 }
 
