d0e1444d3d7bc92b12e648afe3cd13506617ba0a
Nicolas Zapata <nicolas055@hotmail.com>
2023-05-17 16:17:49 -0500

    Added default to entity base

--- LocationService.Domain/EntityBase.cs
+++ LocationService.Domain/EntityBase.cs
@@ -5,7 +5,7 @@ namespace LocationService.Domain;
 public class EntityBase
 {
     public string Id { get; set; }
-    public DateTime LastUpdateDate { get; set; }
+    public DateTime LastUpdateDate { get; set; } = DateTime.UtcNow;
 
-    public string LastUpdateUserId { get; set; }
+    public string LastUpdateUserId { get; set; } = "System";
 }
